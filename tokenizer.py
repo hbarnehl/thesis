5a205aa99bb9be34bad1465cc26bf012bbb7269e
hbarnehl <hennes-michel.barnehl@student.uva.nl>
2022-05-12 21:45:52 +0200

    add creation of tokenlist to tokenizer script

--- tokenizer.py
+++ tokenizer.py
@@ -19,3 +19,15 @@ tokenlist = tokenlist[1:]
 # save to pickle
 with open(f"tokens/tokens.pkl", "wb") as f:
     pickle.dump(tokenlist, f)
+
+# integrate tokenlist into df, useful for later structural topic model and all subsampling
+# load tokenlist
+with open("tokens/tokens.pkl", "rb") as f:
+    tokenlist = pickle.load(f)
+
+# integrate tokenlist back into dataframe
+df = pd.read_csv("dataset_token_ready.csv")
+df = df.assign(tokens=tokenlist)
+
+# save dataset as pickle (csv does not work, because it cannot save the lists of tokens as cell entries)
+df.to_pickle("dataset_with_tokens.pkl")
